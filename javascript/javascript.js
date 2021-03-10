7c7f78de56fbafaffddd593636209d259e37a2b6
Fernando Iván Sevilla Valderrama <feseva99@gmail.com>
2021-03-10 15:29:38 +0100

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,36 @@
+// function darkMode() {
+//     var element = document.getElementById("content1-Login");
+//     if (element.style.backgroundColor = "white") {
+//         element.style.backgroundColor = "grey"
+//     } else {
+//         element.style.backgroundColor = "white"
+
+
+
+//     }
+// }
+
+
+
+// 2 - Pantalla de Ingreso
+// nav bar sideBar w3school
+function w3_open() {
+    var element = document.getElementById("content2-Pantalladeingreso").style.marginLeft;
+    var element2 = document.getElementById("buttonAction").getAttribute("aria-expanded")
+    if (element2 == "false") {
+        document.getElementById("content2-Pantalladeingreso").style.marginLeft = "325px";
+        document.getElementById("content2-Pantalladeingreso").style.marginTop = "20px";
+        document.getElementById("content2-Pantalladeingreso").style.transition = "1s";
+    } else {
+        document.getElementById("content2-Pantalladeingreso").style.marginLeft = "20px";
+        document.getElementById("content2-Pantalladeingreso").style.marginTop = "20px";
+        document.getElementById("content2-Pantalladeingreso").style.transition = "2s";
+
+    }
+}
+
+function w3_close() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+}
